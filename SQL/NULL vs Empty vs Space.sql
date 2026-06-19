26f8f003f63916ad22eb14324679f71223ddb8cc
hoemros200 <george.gomer@gmail.com>
2026-06-18 21:25:16 -0400

    update

---
+++ SQL/NULL vs Empty vs Space.sql
@@ -0,0 +1,12 @@
+-- Detect if a cell is empty or contains one or more spaces.
+
+WITH Orders AS (
+SELECT 1 Id, 'A' Category UNION
+SELECT 2, NULL UNION 
+SELECT 3, ''    UNION
+SELECT 4, '  ' 
+)
+SELECT
+*,
+DATALENGTH (Category) CategoryLen
+FROM Orders
