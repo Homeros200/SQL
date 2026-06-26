14648ff5f19beb7b9d3c5a8a7400be6da1b26959
Homeros200 <george.gomer@gmail.com>
2026-06-25 21:06:41 -0400

    update

--- SQL/Window Ranking ROW_NUMBER Generate Unique IDs.sql
+++ SQL/Window Ranking ROW_NUMBER Generate Unique IDs.sql
@@ -4,10 +4,13 @@ USE SalesDB
 
 
 SELECT 
-*,
-ROW_NUMBER() OVER (ORDER BY OrderID, OrderDate) UniqueID
 
+ROW_NUMBER() OVER (ORDER BY OrderID, OrderDate) UniqueID,
+*
 
 FROM Sales.OrdersArchive
+
+
+
 
 
