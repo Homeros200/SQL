8ca80c71e5b20a1805f130021efc8f1fac8fada7
Homeros200 <george.gomer@gmail.com>
2026-06-28 17:17:03 -0400

    update

---
+++ SQL/Subquery in SELECT Clause.sql
@@ -0,0 +1,19 @@
+USE SalesDB
+
+-- Show the product IDs, product names, prices, and the total number of orders.
+
+--Main Query
+SELECT
+	ProductID,
+	Product,
+	Price,
+	--Subquery
+	(SELECT COUNT(*)  FROM Sales.Orders) AS TotalOrders
+FROM Sales.Products
+
+
+
+
+
+
+
