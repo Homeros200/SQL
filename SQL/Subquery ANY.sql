beed05819bc267ad67878ae14680fe03e3429fe6
Homeros200 <george.gomer@gmail.com>
2026-06-29 15:57:52 -0400

    update

---
+++ SQL/Subquery ANY.sql
@@ -0,0 +1,17 @@
+USE SalesDB
+-- Find female employees whose salaries are greater
+-- than the salaries of any male employees
+
+--Main Query
+SELECT
+EmployeeID,
+FirstName,
+Salary
+FROM Sales.Employees
+WHERE Gender = 'F'
+AND Salary > ANY (SELECT Salary FROM Sales.Employees WHERE Gender = 'M')
+
+
+
+
+
